e1df852d18d6bc018e5bf2c0f968e12a30a240a1
abdalla-hub <114844743+abdalla-hub@users.noreply.github.com>
2023-08-12 18:09:22 -0300

    added project files

---
+++ script.js
@@ -0,0 +1,6 @@
+var elemento = document.getElementById("teste");
+var botao = document.getElementById("testebtn");
+
+botao.addEventListener("click", function() {
+    document.getElementById("teste").style.display = "block";
+});
